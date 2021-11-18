df568b3fad17fc6127c486ef970e9f267d8150ae
Diluka Wittahachchige <diluka.99999@outlook.com>
2021-11-18 10:55:39 +0800

    Add project files.

---
+++ AspDotNetCoreWebApp/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using AspDotNetCoreWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspDotNetCoreWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl
+        = null)
+        {
+            if (ModelState.IsValid)
+            {
+                // work with the model
+            }
+            // At this point, something failed, redisplay form
+            return View(model);
+        }
+    }
+}
